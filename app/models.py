edd201eb3a974383e292517d3e9d8828616b215e
Projoy Roy <proy@burlington.evertz.tv>
2015-11-24 12:13:55 -0500

    added indexes to username and email and followed column in user model

--- app/models.py
+++ app/models.py
@@ -24,15 +24,28 @@ def load_token(token):
         return user
     return None
 
+followers = db.Table('followers',
+                     db.Column(
+                         'follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column(
+                         'followed_id', db.Integer, db.ForeignKey('user.id'))
+                     )
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), nullable=False, unique=True)
-    email = db.Column(db.String(64), nullable=False, unique=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(64), index=True, nullable=False, unique=True)
     pwdhash = db.Column(db.String(64), nullable=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    followed = db.relationship('User',
+                               secondary=followers,
+                               primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
+                               backref=db.backref('followers', lazy='dynamic'),
+                               lazy='dynamic')
 
     def __init__(self, username, email, password=None, about_me=None):
         self.username = username.title()
